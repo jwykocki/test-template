a5bd5aa48f00b9b7e98eb64a52aefd5e06f7a507
Kuba <jakub.wykocki@wp.pl>
2025-03-27 23:35:50 +0100

    add template and jar

---
+++ module1/src/main/java/com/jw/module1/TestTemplate.java
@@ -0,0 +1,8 @@
+package com.jw.module1;
+
+public class TestTemplate {
+
+    public static String render(String input) {
+        return "---RENDER--START -- " + input + "---RENDER--END---";
+    }
+}
